d34768fc5188ce2e00b998a9831263bbb1d38784
Koushik Kothagal <koush.s@gmail.com>
2019-02-16 23:27:36 -0800

    06. Using BeforeEach

--- src/test/java/io/javabrains/utils/MathUtilsTest.java
+++ src/test/java/io/javabrains/utils/MathUtilsTest.java
@@ -2,13 +2,20 @@ package io.javabrains.utils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
+	
+	private MathUtils mathUtils;
+	
+	@BeforeEach
+	void initEach() {
+		mathUtils = new MathUtils();
+	}
 
 	@Test
 	void testAdd() {
-		MathUtils mathUtils = new MathUtils();
 		int expected = 2;
 		int actual = mathUtils.add(1, 1);
 		assertEquals(expected, actual, 
@@ -17,14 +24,12 @@ class MathUtilsTest {
 	
 	@Test 
 	void computeCircleArea() {
-		MathUtils mathUtils = new MathUtils();
 		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), 
 				"Should return right circle area");
 	}
 	
 	@Test
 	void testDivide() {
-		MathUtils mathUtils = new MathUtils();
 		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), 
 				"Divide should throw ArithmeticException when denominator is zero");
 	}
