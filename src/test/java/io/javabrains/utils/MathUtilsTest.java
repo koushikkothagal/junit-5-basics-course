16919d9f434a834175b5725e377bc017c6099e4f
Koushik Kothagal <koush.s@gmail.com>
2019-02-16 22:18:55 -0800

    03. Testing the add method

--- src/test/java/io/javabrains/utils/MathUtilsTest.java
+++ src/test/java/io/javabrains/utils/MathUtilsTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 class MathUtilsTest {
 
 	@Test
-	void test() {
-		System.out.println("This test ran");
+	void testAdd() {
+		MathUtils mathUtils = new MathUtils();
+		int expected = 2;
+		int actual = mathUtils.add(1, 1);
+		assertEquals(expected, actual, "Add method should return the sum of two numbers");
 	}
-
 }
