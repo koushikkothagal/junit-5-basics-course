1217404199e90fb2e5185433b2b8ba1669e6193f
Koushik Kothagal <koush.s@gmail.com>
2019-02-16 16:56:16 -0800

    02. Creating a test class

---
+++ src/main/java/io/javabrains/utils/MathUtils.java
@@ -0,0 +1,21 @@
+package io.javabrains.utils;
+
+public class MathUtils {
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a/b;
+	}
+	
+}
---
+++ src/test/java/io/javabrains/utils/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package io.javabrains.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		System.out.println("This test ran");
+	}
+
+}
