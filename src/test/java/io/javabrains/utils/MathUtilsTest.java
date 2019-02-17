5e8c35611993525f6c9c8e1914b7f6cc6bca71ef
Koushik Kothagal <koush.s@gmail.com>
2019-02-16 22:43:07 -0800

    05. Using assertThrows

--- src/test/java/io/javabrains/utils/MathUtilsTest.java
+++ src/test/java/io/javabrains/utils/MathUtilsTest.java
@@ -11,12 +11,21 @@ class MathUtilsTest {
 		MathUtils mathUtils = new MathUtils();
 		int expected = 2;
 		int actual = mathUtils.add(1, 1);
-		assertEquals(expected, actual, "Add method should return the sum of two numbers");
+		assertEquals(expected, actual, 
+				"Add method should return the sum of two numbers");
 	}
 	
 	@Test 
 	void computeCircleArea() {
 		MathUtils mathUtils = new MathUtils();
-		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), 
+				"Should return right circle area");
+	}
+	
+	@Test
+	void testDivide() {
+		MathUtils mathUtils = new MathUtils();
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), 
+				"Divide should throw ArithmeticException when denominator is zero");
 	}
 }
